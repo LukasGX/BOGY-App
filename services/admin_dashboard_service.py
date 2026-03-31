d7212aec9d714d6c341a14c62725f091969aa6f7
LukasGX <lukas@grambs.info>
2026-03-31 15:29:18 +0200

    wlan codes view in dashboard

--- services/admin_dashboard_service.py
+++ services/admin_dashboard_service.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from api.v1.deps import get_db
 from services.data_service import get_classes_s, get_users_s
+from services.wlan_service import get_wlan_codes
 from definitions import templates
 
 def root_s(request, session_data):
@@ -9,11 +11,27 @@ def root_s(request, session_data):
 
         users_raw = get_users_s()
         users = users_raw.get("users", [])
+
+        wlan_codes_raw = get_wlan_codes(session_data)
+        wlan_codes = wlan_codes_raw.get("codes", [])
+
+        for code in wlan_codes:
+            if code.get('expiry'):
+                try:
+                    # ISO-String parsen (Z → +00:00)
+                    expiry_str = code['expiry'].replace('Z', '+00:00')
+                    dt = datetime.strptime(expiry_str, '%Y-%m-%d %H:%M:%S')
+                    code['expiry_formatted'] = dt.strftime('%d.%m.%Y %H:%M')
+                except ValueError:
+                    code['expiry_formatted'] = code['expiry']
+            else:
+                code['expiry_formatted'] = 'Kein Ablaufdatum'
             
         context = {
             "request": request,
             "username": session_data.get("username", ""),
             "classes": classes[0:4],
             "users": users[0:4],
+            "wlan_codes": wlan_codes[0:4]
         }
         return templates.TemplateResponse("dashboard.html", context)
